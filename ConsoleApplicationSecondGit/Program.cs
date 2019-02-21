efcd73cac3ef0de22fb07c933c1531953a8a1a6a
bebebe <hahaha@gmail.com>
2019-02-21 10:37:16 +0200

    Read Data. Console Project

--- ConsoleApplicationSecondGit/Program.cs
+++ ConsoleApplicationSecondGit/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApplicationSecondGit
     {
         public static void Main(string[] args)
         {
+            string jazzUrl = Environment.GetEnvironmentVariable("JAZZ_URL");
+            string jazzLogin = Environment.GetEnvironmentVariable("JAZZ_LOGIN");
+            string jazzPassword = Environment.GetEnvironmentVariable("JAZZ_PASSWORD");
+            string jazzPin = Environment.GetEnvironmentVariable("JAZZ_PIN");
+            string jazzDatabase = Environment.GetEnvironmentVariable("JAZZ_DATABASE");
+            //
+            Console.WriteLine("jazzUrl = " + jazzUrl);
+            Console.WriteLine("jazzLogin = " + jazzLogin);
+            Console.WriteLine("jazzPassword = " + jazzPassword);
+            Console.WriteLine("jazzPin = " + jazzPin);
+            Console.WriteLine("jazzDatabase = " + jazzDatabase);
+            //
             Console.WriteLine("Application Done");
         }
     }
